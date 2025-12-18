c0145994810d3449cb40b3f63029d74014952424
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-18 10:12:50 +0100

    tot domande si mostrano in grafico

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,6 +1,24 @@
-let xValues = ["Correct", "Wrong"];
-let yValues = [55, 49];
+
+
+// LOGICA per mostrare il numero e percentuale di domande giuste e sbagliate
+// domande giuste = totale domande / numero domande giuste
+// domande sbagliate = totale domande / numero domande sbagliate
+
+// prendi parametri della query string sotto la proprietà "risultatiTest"
+
+const parametriUrl = new URLSearchParams(window.location.search);
+const risultatiTestStr = parametriUrl.get('risultatiTest');
+
+const risultatiTest = JSON.parse(risultatiTestStr)
+
+
+// questa libreria non ha bisogno di un pre-calcolo 
+// su base 100, ma calcola automaticamente la base 100 derivandola
+// da tot domande giuste e tot domande sbagliate
+let xValues = ["Wrong", "Correct"];
+let yValues = [risultatiTest.totDomandeSbagliate, risultatiTest.totDomandeGiuste];
 let barColors = ["#b91d47", "#00aba9"];
+
 window.addEventListener("load", () => {
   const graph = document.getElementById("#graph"); // inserisci codice qui
   new Chart("myChart", {
@@ -21,17 +39,3 @@ window.addEventListener("load", () => {
     },
   });
 });
-
-
-// LOGICA per mostrare il numero e percentuale di domande giuste e sbagliate
-// domande giuste = totale domande / numero domande giuste
-// domande sbagliate = totale domande / numero domande sbagliate
-
-// prendi parametri della query string sotto la proprietà "risultatiTest"
-
-const parametriUrl = new URLSearchParams(window.location.search);
-const risultatiTestStr = parametriUrl.get('risultatiTest');
-
-const risultatiTest = JSON.parse(risultatiTestStr)
-
-console.log(risultatiTest)
