5f9b2bc51d1c0796d33d6c829bb1f738fdb7f306
Giuseppe Tavella <giuseppetavella8@gmail.com>
2025-12-18 10:21:19 +0100

    refactor logica risultati test

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,41 +1,67 @@
-
-
-// LOGICA per mostrare il numero e percentuale di domande giuste e sbagliate
-// domande giuste = totale domande / numero domande giuste
-// domande sbagliate = totale domande / numero domande sbagliate
-
-// prendi parametri della query string sotto la proprietà "risultatiTest"
-
-const parametriUrl = new URLSearchParams(window.location.search);
-const risultatiTestStr = parametriUrl.get('risultatiTest');
-
-const risultatiTest = JSON.parse(risultatiTestStr)
-
-
-// questa libreria non ha bisogno di un pre-calcolo 
-// su base 100, ma calcola automaticamente la base 100 derivandola
-// da tot domande giuste e tot domande sbagliate
-let xValues = ["Wrong", "Correct"];
-let yValues = [risultatiTest.totDomandeSbagliate, risultatiTest.totDomandeGiuste];
-let barColors = ["#b91d47", "#00aba9"];
-
-window.addEventListener("load", () => {
-  const graph = document.getElementById("#graph"); // inserisci codice qui
-  new Chart("myChart", {
-    type: "doughnut",
-    data: {
-      datasets: [
-        {
-          backgroundColor: barColors,
-          data: yValues,
-        },
-      ],
-    },
-    options: {
-      cutoutPercentage: 70,
-      tooltips: {
-        enabled: false,
-      },
-    },
+function main() {
+  window.addEventListener("load", () => {
+    quandoPaginaCarica();
   });
-});
+}
+main() 
+
+
+function quandoPaginaCarica() {
+  const risultatiTest = ottieniRisultatiTestDaUrl();
+  mostraRisultatiInGrafico(risultatiTest);
+  mostraPercentualiTesto(risultatiTest)
+}
+
+
+function mostraPercentualiTesto(risultatiTest) {
+  
+}
+
+
+function mostraRisultatiInGrafico(risultatiTest) {
+  // questa libreria non ha bisogno di un pre-calcolo
+  // su base 100, ma calcola automaticamente la base 100 derivandola
+  // da tot domande giuste e tot domande sbagliate
+  let xValues = ["Wrong", "Correct"];
+  let yValues = [risultatiTest.totDomandeSbagliate, risultatiTest.totDomandeGiuste];
+  let barColors = ["#b91d47", "#00aba9"];
+
+  window.addEventListener("load", () => {
+    const graph = document.getElementById("#graph"); // inserisci codice qui
+    new Chart("myChart", {
+      type: "doughnut",
+      data: {
+        datasets: [
+          {
+            backgroundColor: barColors,
+            data: yValues,
+          },
+        ],
+      },
+      options: {
+        cutoutPercentage: 70,
+        tooltips: {
+          enabled: false,
+        },
+      },
+    });
+  });
+}
+
+
+function ottieniRisultatiTestDaUrl() {
+  // LOGICA per mostrare il numero e percentuale di domande giuste e sbagliate
+  // domande giuste = totale domande / numero domande giuste
+  // domande sbagliate = totale domande / numero domande sbagliate
+  // prendi parametri della query string sotto la proprietà "risultatiTest"
+  const parametriUrl = new URLSearchParams(window.location.search);
+  const risultatiTestStr = parametriUrl.get("risultatiTest");
+
+  const risultatiTest = JSON.parse(risultatiTestStr);
+
+  return {
+    totDomande: risultatiTest.totDomande,
+    totDomandeSbagliate: risultatiTest.totDomandeSbagliate,
+    totDomandeGiuste: risultatiTest.totDomandeGiuste,
+  };
+}
